83d309a2c51c1b5192adcf149a0849e2dbfd2098
joy <joy.lai@zg.com>
2019-07-24 12:45:10 +0800

    fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,19 +5,20 @@ declare global {
         }
     }
 }
-export * from './helpers/browser_window_listener';
-export * from './helpers/context_menu';
-export * from './helpers/document_listener';
-export * from './helpers/electron_route_factory';
-export * from './utils/app';
-export * from './utils/clipboard';
-export * from './utils/cookie';
-export * from './utils/dialog';
-export * from './utils/ipc_main';
-export * from './utils/ipc_render';
-export * from './utils/power';
-export * from './utils/screen';
-export * from './utils/session';
-export * from './utils/shell';
-export * from './utils/shortcut';
-export * from './utils/window';
+import BrowserWindowListenerHelper from './helpers/browser_window_listener';
+import ContextMenuHelper from './helpers/context_menu';
+import DocumentListenerHelper from './helpers/document_listener';
+import ElectronRouteFactoryHelper from './helpers/electron_route_factory';
+import AppUtil from './utils/app';
+import ClipboardUtil from './utils/clipboard';
+import CookieUtil from './utils/cookie';
+import DialogUtil from './utils/dialog';
+import IpcMainUtil from './utils/ipc_main';
+import IpcRenderUtil from './utils/ipc_render';
+import PowerUtil from './utils/power';
+import ScreenUtil from './utils/screen';
+import SessionUtil from './utils/session';
+import ShellUtil from './utils/shell';
+import ShortcutUtil from './utils/shortcut';
+import WindowUtil from './utils/window';
+export { BrowserWindowListenerHelper, ContextMenuHelper, DocumentListenerHelper, ElectronRouteFactoryHelper, AppUtil, ClipboardUtil, CookieUtil, DialogUtil, IpcMainUtil, IpcRenderUtil, PowerUtil, ScreenUtil, SessionUtil, ShellUtil, ShortcutUtil, WindowUtil, };
